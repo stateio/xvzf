d44ffc901a8ec53794b131a958d4c52075bded2e
Phill MV <phillmv@okayfail.com>
2014-08-12 23:01:11 -0400

    Fixed deploy config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,10 @@ namespace :deploy do
  desc "Linking application specific directories"
   task :create_symlinks do
     run "ln -nfs #{shared_path}/ohhi/ #{release_path}/_site/ohhi"
-    run "ln -nfs #{shared_path}/mp3/ #{release_path}/_podcast/mp3"
     run "ln -nfs #{release_path}/_site/ohhi #{release_path}/_site/public"
-    run "ln -nfs #{release_path}/_podcast/podcast.atom #{release_path}/_site/"
+    
+    run "ln -nfs #{release_path}/_site/podcast/ #{release_path}/_podcast"
+    run "ln -nfs #{shared_path}/mp3/ #{release_path}/_podcast/mp3"
   end
 
 end
